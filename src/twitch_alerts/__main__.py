ca185e8fa2bb0e409114954d89edd843c18c74a0
Preocts <preocts@preocts.com>
2025-08-22 00:03:50 -0400

    Get twitch bearer token

--- src/twitch_alerts/__main__.py
+++ src/twitch_alerts/__main__.py
@@ -1,7 +1,11 @@
 from __future__ import annotations
 
+import os
+import logging
 import dataclasses
 import tomllib
+import time
+from typing import Any
 
 import httpx
 import dotenv
@@ -11,10 +15,31 @@ SCAN_FREQUENCY_SECONDS = 300  # Five minutes
 
 dotenv.load_dotenv()
 
+logging.basicConfig(level="INFO")
+logger = logging.getLogger(__name__)
+
+
 @dataclasses.dataclass(frozen=True, slots=True)
 class Config:
     discord_webhook_url: str
     twitch_channel_names: frozenset[str]
+
+
+@dataclasses.dataclass(frozen=True, slots=True)
+class Bearer:
+    access_token: str
+    expires_in: int
+    token_type: str
+    expires_at: int = 0
+
+    @classmethod
+    def parse_response(cls, response: dict[str, Any]) -> Bearer:
+        expires_at = int(time.time() + response["expires_in"])
+        return Bearer(**response, expires_at=expires_at)
+
+    @property
+    def expired(self) -> bool:
+        return time.time() >= self.expires_at
 
 
 def load_config() -> Config:
@@ -26,3 +51,25 @@ def load_config() -> Config:
         discord_webhook_url=raw_config["discord_webhook_url"],
         twitch_channel_names=frozenset(raw_config["twitch_channel_names"]),
     )
+
+
+def get_bearer_token() -> Bearer | None:
+    """Get Twitch API bearer via client credential grant flow."""
+    logger.info("Getting bearer token...")
+
+    url = "https://id.twitch.tv/oauth2/token"
+    payload = {
+        "client_id": os.getenv("TWITCH_ALERT_CLIENT_ID", "[clientId]"),
+        "client_secret": os.getenv("TWITCH_ALERT_CLIENT_SECRET", "[clientSecret]"),
+        "grant_type": "client_credentials",
+    }
+
+    response = httpx.post(url, data=payload, timeout=3)
+
+    if not response.is_success:
+        logger.critical("Failed to get bearer token: %s", response.text)
+        return None
+
+    logger.info("Bearer token response got.")
+
+    return Bearer.parse_response(response.json())
--- tests/twitch_alerts_test.py
+++ tests/twitch_alerts_test.py
@@ -1,19 +1,36 @@
 from __future__ import annotations
 
+import os
+import json
 from unittest.mock import patch
+
+import pytest
+import vcr  # type: ignore
+
 
 from twitch_alerts import __main__ as main
 
-@pytest.fixture(autouse=True)
-def mock_env() -> None:
-    if "TWITCH_ALERT_RECORDING" not in os.environ:
-        os.environ["TWITCH_ALERT_CLIENT_ID"] = "[clientId]"
-        os.environ["TWITCH_ALERT_CLIENT_Secret"] = "[clientSecret]"
+
+def filter_access_token(response):
+    json_response = json.loads(response["body"]["string"])
+    json_response["access_token"] = "mockToken"
+    response["body"]["string"] = json.dumps(json_response)
+    return response
+
+
+recorder = vcr.VCR(
+    cassette_library_dir="tests/cassettes",
+    record_mode="once",
+    filter_headers=[],
+    filter_post_data_parameters=["client_id", "client_secret"],
+    before_record_response=filter_access_token,
+    match_on=["method", "url"],
+)
 
 
 @pytest.fixture
 def config() -> main.Config:
-    return main.Config("mock", "mockery", "https://example.com", frozenset())
+    return main.Config("https://example.com", frozenset())
 
 
 def test_load_config() -> None:
@@ -22,3 +39,19 @@ def test_load_config() -> None:
 
     assert result.discord_webhook_url == "https://example.com"
     assert result.twitch_channel_names == {"all", "the", "streamers"}
+
+
+@recorder.use_cassette()
+def test_get_bearer_token():
+    bearer = main.get_bearer_token()
+
+    assert bearer
+    assert not bearer.expired
+
+
+@recorder.use_cassette()
+def test_get_bearer_token_failure():
+    with patch.dict(os.environ, {"TWITCH_ALERT_CLIENT_ID": "MOCK"}):
+        bearer = main.get_bearer_token()
+
+    assert bearer is None
