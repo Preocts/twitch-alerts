f2ae6722625dbee3497b11b4d356abd49e3f4c91
Preocts <preocts@preocts.com>
2025-08-25 23:45:59 -0400

    Remove import side-effects

--- src/twitch_alerts/__main__.py
+++ src/twitch_alerts/__main__.py
@@ -1,19 +1,55 @@
 from __future__ import annotations
 
+import logging
+import os
+
+from eggviron import Eggviron
+from eggviron import EnvFileLoader
+
 from twitch_alerts import _twitch_alerts
 
+_REQUIRED_ENVIRONS = {
+    "TWITCH_ALERT_CLIENT_SECRET",
+}
 
-def run() -> None:
+
+def _init() -> None:
+    """Setup required run-time environment and logging."""
+    missing_required_environ = bool(_REQUIRED_ENVIRONS - set(os.environ.keys()))
+
+    try:
+        Eggviron().load(EnvFileLoader())
+
+    except (FileNotFoundError, OSError):
+        if missing_required_environ:
+            print(
+                "Could not load the expected '.env' file and required environment variables are not present:"
+            )
+            print("\n".join(sorted(_REQUIRED_ENVIRONS)))
+            raise SystemExit(1)
+
+    logging.basicConfig(
+        level=os.getenv("TWITCH_ALERTS_LOGGING", "INFO"),
+        format="%(levelname)s [%(asctime)s] - %(message)s",
+    )
+
+
+def run() -> int:
+    _init()
     try:
         _twitch_alerts.run()
 
     except KeyboardInterrupt:
         pass
 
+    return 0
 
-def run_once() -> None:
+
+def run_once() -> int:
+    _init()
     _twitch_alerts.run(loop_flag=False)
+    return 0
 
 
 if __name__ == "__main__":
-    run_once()
+    raise SystemExit(run_once())
